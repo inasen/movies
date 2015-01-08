2030d5c0b4e6a2b0f90ebecdc2b68b5616e37005
inasens <inigoasensio1@gmail.com>
2015-01-08 09:34:45 +0000

    heroku production

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,12 +2,5 @@ class Purchase < ActiveRecord::Base
 	belongs_to :movie
   belongs_to :buyer, class_name: 'User'
 
-	def change
-    create_table :purchases do |t|
-      t.integer :movie_id
-      t.integer :buyer_id
-      t.timestamps
-    end
-    add_index :purchases, [:movie_id, :buyer_id], unique: true
-  end
+	
 end
